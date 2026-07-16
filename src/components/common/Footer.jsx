640fc1d8384b742dac2f5d3873aed138a1eed9a6
Val-Antin1 <valentinlyon205@gmail.com>
2026-07-16 20:20:50 +0200

    frontend

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -24,7 +24,7 @@ const sportLinks = [
 const Footer = () => {
   const { t } = useTranslation();
   return (
-    <footer className="bg-black text-white">
+    <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           {/* Brand */}
